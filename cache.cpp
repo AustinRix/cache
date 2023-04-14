17d4ff130e870669cc6ab755f80376039675f9e7
Austin Rix <ajr331@txstate.edu>
2023-04-14 01:29:07 -0500

    i/o

--- cache.cpp
+++ cache.cpp
@@ -3,6 +3,8 @@
 #include<cstdlib>
 using namespace std;
 
+const int MAX_SIZE = 1000;
+
 class Entry {
 public:
   Entry();
@@ -52,24 +54,54 @@ private:
 
 int main(int argc, char*argv[]) {
 
-  /* check if the correct number of arguments have been passed; bail otherwise */
-  if (argc < 4) {
-    cout << "Usage: " << endl;
-    cout << "   ./cache num_entries associativity filename" << endl;
+    /* check if the correct number of arguments have been passed; bail otherwise */
+    if (argc < 4) {
+        cout << "Usage: " << endl;
+        cout << "   ./cache num_entries associativity filename" << endl;
+        return 0;
+    }
+
+    if (argc < 2) {
+        cout << "Usage: " << endl;
+        cout << "   ./file_process filename" << endl;
+        return 0;
+    }
+    /* get args; all args come in as strings; need to convert to int with atoi() */
+    unsigned entries = atoi(argv[1]);
+    unsigned assoc = atoi(argv[2]);
+    string filename = argv[3];
+    string input_filename = filename + "_input";
+    string output_filename = filename + "_output";
+    ofstream output;
+    ifstream input;
+
+    // input start
+    input.open(input_filename);
+    if (!input.is_open()) {
+        cerr << "Could not open input file " << input_filename << ". Exiting ..." << endl;
+        exit(0);
+    }
+
+    unsigned long *nums = new unsigned long[MAX_SIZE];
+    int count = 0;
+    while (!input.eof() && count < MAX_SIZE) {
+        input >> nums[count];
+        count++;
+    }
+
+    input.close();
+
+    // output start
+    output.open(output_filename);
+    for (int i = 0; i < count; i++)
+        output << "ADDR : " << nums[i] << endl;
+    output.close();
+
+    /* print the args */
+    cout << "Number of entries: " << entries << endl;
+    cout << "Associativity: " << assoc << endl;
+    cout << "Input File Name: " << filename << endl;
+
     return 0;
-  }
-
-  /* get args; all args come in as strings; need to convert to int with atoi() */
-  unsigned entries = atoi(argv[1]);
-  unsigned assoc = atoi(argv[2]);
-
-  string input_filename = argv[3];
-
-  /* print the args */
-  cout << "Number of entries: " << entries << endl;
-  cout << "Associativity: " << assoc << endl;
-  cout << "Input File Name: " << input_filename << endl;
-
-  return 0;
 }
 
